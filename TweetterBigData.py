0aa178ba7f2b662332f797f28123999be1fef3cd
ascarneiro <alansoarescarneiro21@gmail.com>
2019-06-22 20:35:32 -0300

    feat: commits

--- TweetterBigData.py
+++ TweetterBigData.py
@@ -9,10 +9,6 @@ class TweetterBigData(object):
 
     def __init__(self):
         # Chaves e tokens. Cadastrar API no Tweeter
-        consumer_key = '7BaiwVN9g6jQVRP72qUqyRDlB'
-        consumer_secret = 'rDFun0w15jVcLmO8Ctk7g04Xeblx9MZJLEPLneEXoqqSuNBehk'
-        access_token = '876812531481882628-KSUZx4Qb29SCYnnEErOF4ATWbei56nC'
-        access_token_secret = 'xqsO7f4f8tGopFfcTZovAJyqPp1OY1Ud0rnfpjuEiXun7'
 
         # autenticacao
         try:
